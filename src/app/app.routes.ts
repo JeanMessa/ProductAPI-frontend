e2e0afd8c501e9798c230ef87e9219e1854a10d4
JeanMessa <jeancarlosmessa@gmail.com>
2025-07-17 15:51:02 -0300

    feat: add auth-guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { App } from './app';
+import { AuthGuardService } from './services/auth-guard.service';
 
 export const routes: Routes = [
     {
         path: "login",
-        component: LoginComponent
+        component: LoginComponent,
+        canActivate: [AuthGuardService],
+        data:{
+            requireNotAuthenticated: true
+        }
     }
+
 ];
