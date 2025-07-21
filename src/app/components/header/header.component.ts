bc35fac1ec2632b05cccbe83247b9e763edf5c1c
JeanMessa <jeancarlosmessa@gmail.com>
2025-07-21 15:59:27 -0300

    feat: add menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  username:string | null = sessionStorage.getItem('username');
+  isAdmin:boolean = sessionStorage.getItem('role') == "ADMIN";
 
   constructor(private router:Router){}
 
